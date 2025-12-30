70092e4e384f4984c452b0469238dda34bde2e5b
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:43:26 -0300

    feat: add styling

--- src/app/components/Badge.tsx
+++ src/app/components/Badge.tsx
@@ -1,10 +1,22 @@
+import { User_Type } from "../types/Filter";
+
 type BadgeProps = {
   children: React.ReactNode;
-  color: "first" | "second" | "third" | "fourth" | "fifth";
+  color: User_Type;
+  active: boolean;
+  type: "display" | "clickable";
 };
 
-const Badge = ({ children, color }: BadgeProps) => {
-  return <div className={`badge badge-${color}`}>{children}</div>;
+const Badge = ({ children, color, active, type }: BadgeProps) => {
+  return (
+    <div
+      className={`badge badge-${color} ${!active ? "badge-not-used" : ""} ${
+        type === "clickable" ? "badge-clickable" : ""
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Badge;
