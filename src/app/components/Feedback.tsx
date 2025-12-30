9c75ffbeacdb918db3c041568407d1ccd58f22de
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:46:12 -0300

    feat: feedback

---
+++ src/app/components/Feedback.tsx
@@ -0,0 +1,12 @@
+const Feedback = () => {
+  return (
+    <div className="feedback">
+      <p className="title">No users found!</p>
+      <p className="text-medium">
+        Try adjusting your search or filter to find what you're looking for.
+      </p>
+    </div>
+  );
+};
+
+export default Feedback;
