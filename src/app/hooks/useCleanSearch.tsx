66e26b2e079e76f6e80fc4e887e0726a520ffd8b
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:43:56 -0300

    feat: improve clean search

--- src/app/hooks/useCleanSearch.tsx
+++ src/app/hooks/useCleanSearch.tsx
@@ -1,12 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const useCleanSearch = (
   searchText: string,
   setSearchClick: (click: boolean) => void
 ) => {
+  const prevSearchText = useRef(searchText);
+
   useEffect(() => {
     if (searchText === "") {
       setSearchClick(false);
     }
+
+    if (searchText && searchText !== prevSearchText.current) {
+      setSearchClick(false);
+    }
+
+    prevSearchText.current = searchText;
   }, [searchText, setSearchClick]);
 };
