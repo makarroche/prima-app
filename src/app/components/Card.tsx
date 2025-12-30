5968ec54a2a3af80060c9ddfb36862113d43946d
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:46:21 -0300

    feat: card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,23 +1,71 @@
+import { useState } from "react";
 import Badge from "./Badge";
 import Button from "./Button";
+import Modal from "./Modal";
 
 type CardProps = {
   name: string;
   role: string;
   team: string;
   email: string;
+  user_type: string;
+  otherDetails: string;
 };
 
-const Card = ({ name, role, team, email }: CardProps) => {
+const Card = ({
+  name,
+  role,
+  team,
+  email,
+  user_type,
+  otherDetails,
+}: CardProps) => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="card">
-      <Badge color="first">Badge</Badge>
-      <p className="title">{name}</p>
-      <p className="text-large">{role}</p>
-      <p className="text-medium">Team: {team}</p>
-      <p className="text-medium">Contact Information:</p>
-      <p className="url">{email}</p>
-      <Button type="button-slim">View details</Button>
+      <Badge
+        color={
+          user_type as
+            | "admin"
+            | "editor"
+            | "viewer"
+            | "guest"
+            | "owner"
+            | "inactive"
+        }
+        active={true}
+        type="display"
+      >
+        {user_type}
+      </Badge>
+      <span className="title">{name}</span>
+      <span className="text-medium">{role}</span>
+      <div className="team-container">
+        <span className="text-small">Team:</span>
+        <span className="text-small-bold">{team}</span>
+      </div>
+      <div className="team-container">
+        <span className="text-small">Contact Information:</span>
+        <span className="url">{email}</span>
+      </div>
+      <Button
+        type="button-slim"
+        aria-label="view details"
+        onClick={() => setModalOpen(true)}
+      >
+        View details
+      </Button>
+      {modalOpen && (
+        <Modal
+          name={name}
+          role={role}
+          team={team}
+          email={email}
+          otherDetails={otherDetails}
+          user_type={user_type}
+          setModalOpen={setModalOpen}
+        />
+      )}
     </div>
   );
 };
