9c73a4e755ff8abfdc6a43e3d8deee411809db9d
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 08:42:47 -0300

    feat: add constants

---
+++ src/app/constants.tsx
@@ -0,0 +1,8 @@
+export const filters = [
+  { role: "Admin", color: "first" },
+  { role: "Editor", color: "second" },
+  { role: "Viewer", color: "third" },
+  { role: "Guest", color: "fourth" },
+  { role: "Owner", color: "fifth" },
+  { role: "Inactive", color: "fifth" },
+];
