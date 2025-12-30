ace8a23e2113ae144d7b8bac6de62778e01d37d8
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:44:10 -0300

    feat: modal

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,36 +1,68 @@
+import { useEffect } from "react";
 import Badge from "./Badge";
 import Button from "./Button";
+import { useHandleEscape } from "../hooks/useHandleEscape";
+import { useHandleEnter } from "../hooks/useHandleEnter";
 
 type ModalProps = {
   name: string;
   role: string;
   team: string;
-  website: string;
-  contactInfo: string;
+  email: string;
   otherDetails: string;
+  user_type: string;
+  setModalOpen: (isOpen: boolean) => void;
 };
 
 const Modal = ({
   name,
   role,
   team,
-  website,
-  contactInfo,
+  email,
   otherDetails,
+  user_type,
+  setModalOpen,
 }: ModalProps) => {
+  useHandleEscape(setModalOpen);
+  useHandleEnter(setModalOpen);
+
   return (
-    <div className="modal">
-      <Badge color="first">Admin</Badge>
-      <p className="title">{name}</p>
-      <p className="text-large">{role}</p>
-      <p className="text-medium">Team: {team}</p>
-      <p className="subtitle">{website}</p>
-      <p className="text-medium">Contact Information:</p>
-      <p className="url">{contactInfo}</p>
-      <p className="text-medium">Other Details:</p>
-      <p className="subtitle">{otherDetails}</p>
-      <div className="modal-close-container">
-        <Button type="button-slim">Close</Button>
+    <div className="modal-overlay">
+      <div className="modal" role="dialog">
+        <Badge
+          color={
+            user_type as
+              | "admin"
+              | "editor"
+              | "viewer"
+              | "guest"
+              | "owner"
+              | "inactive"
+          }
+          active={true}
+          type="display"
+        >
+          {user_type}
+        </Badge>
+        <span className="title">{name}</span>
+        <span className="text-medium">{role}</span>
+        <div className="team-container">
+          <span className="text-small">Team:</span>
+          <span className="text-small-bold">{team}</span>
+        </div>
+        <div className="team-container">
+          <span className="text-small">Contact Information:</span>
+          <span className="url">{email}</span>
+        </div>
+        <div className="team-container">
+          <span className="text-small">Other Details:</span>
+          <span className="text-small-bold">{otherDetails}</span>
+        </div>
+        <div className="modal-close-container">
+          <Button type="button-slim" onClick={() => setModalOpen(false)}>
+            Close
+          </Button>
+        </div>
       </div>
     </div>
   );
