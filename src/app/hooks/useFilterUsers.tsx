3708ba123c2fa802d5a9496e373b5020f85e3085
makarroche <49964534+makarroche@users.noreply.github.com>
2025-12-30 16:45:48 -0300

    chore: clean

--- src/app/hooks/useFilterUsers.tsx
+++
@@ -1,7 +0,0 @@
-import { user_information } from "../../user_info/information";
-
-export const useFilterUsers = (searchText: string) => {
-  return user_information.filter((user) =>
-    user.name.toLowerCase().includes(searchText.toLowerCase())
-  );
-};
